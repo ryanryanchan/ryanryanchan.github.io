eb1be5f5e16142f81f951e3dc45e1830c558de0c
RYAN CHAN <ryanchan1622@yahoo.com>
2018-09-26 03:49:37 +0900

    logos
    added logos and links

--- js/init.js
+++ js/init.js
@@ -1,9 +1,8 @@
-  $(document).ready(function(){
-$('.carousel').carousel();
-   });
+ $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
 
-(function($){
-  $(function(){
-    $('.button-collapse').sideNav();
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+$(document).ready(
+  function(){
+    $('.carousel').carousel();
+});
